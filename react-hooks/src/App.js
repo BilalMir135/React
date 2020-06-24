81fd68ff88cb0dcbe1c739e0670e7ee5b88aa919
Bilal Mir <bilalmir135@gmail.com>
2020-06-24 14:58:04 +0500

    Update React Hooks

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,63 +1,63 @@
 import React from 'react';
 import './App.css';
-//import HookCounter from './components/01_useState/HookCounter';
-//import HookCounterTwo from './components/01_useState/HookCounterTwo';
-//import HookCounterThree from './components/01_useState/HookCounterThree';
-//import HookCounterFour from './components/01_useState/HookCounterFour';
-//import HookEffect from './components/02_useEffect/HookEffect';
-//import HookEffectTwo from './components/02_useEffect/HookEffectTwo';
-//import HookEffectThree from './components/02_useEffect/HookEffectThree';
-//import HookEffectFour from './components/02_useEffect/HookEffectFour';
-//import HookEffectFive from './components/02_useEffect/HookEffectFive';
-//import FetchData from './components/02_useEffect/FetchData';
-//import FetchDataTwo from './components/02_useEffect/FetchDataTwo';
-//import ComponentA from './components/03_useContext/ComponentA';
-//import { NameContext, AgeContext } from './components/03_useContext/Context';
-//import HookReducer from './components/04_useReducer/HookReducer';
-//import HookReducerTwo from './components/04_useReducer/HookReducerTwo';
-//import Main from './components/05_UR_UC/Main';
-//import FetchingData from './components/06_UR_US/FetchingData';
-//import FetchingDataTwo from './components/06_UR_US/FetchingDataTwo';
-//import ParentComponent from './components/07_useCallback/ParentComponent';
-//import Counter from './components/08_useMemo/Counter';
-//import FocusInput from './components/09_useRef/FocusInput';
-//import HookTimer from './components/09_useRef/HookTimer';
-//import DocumentTitle from './components/10_CustomHooks/01_useDT/DocumentTitle';
-//import DocumentTitleTwo from './components/10_CustomHooks/01_useDT/DocumentTitleTwo';
-//import Counter from './components/10_CustomHooks/02_useCounter/Counter';
+import HookCounter from './components/01_useState/HookCounter';
+import HookCounterThree from './components/01_useState/HookCounterThree';
+import HookCounterFour from './components/01_useState/HookCounterFour';
+import HookEffect from './components/02_useEffect/HookEffect';
+import HookCounterTwo from './components/01_useState/HookCounterTwo';
+import HookEffectTwo from './components/02_useEffect/HookEffectTwo';
+import HookEffectThree from './components/02_useEffect/HookEffectThree';
+import HookEffectFour from './components/02_useEffect/HookEffectFour';
+import HookEffectFive from './components/02_useEffect/HookEffectFive';
+import FetchData from './components/02_useEffect/FetchData';
+import FetchDataTwo from './components/02_useEffect/FetchDataTwo';
+import ComponentA from './components/03_useContext/ComponentA';
+import { NameContext, AgeContext } from './components/03_useContext/Context';
+import HookReducer from './components/04_useReducer/HookReducer';
+import HookReducerTwo from './components/04_useReducer/HookReducerTwo';
+import Main from './components/05_UR_UC/Main';
+import FetchingData from './components/06_UR_US/FetchingData';
+import FetchingDataTwo from './components/06_UR_US/FetchingDataTwo';
+import ParentComponent from './components/07_useCallback/ParentComponent';
+import Counter from './components/08_useMemo/Counter';
+import FocusInput from './components/09_useRef/FocusInput';
+import HookTimer from './components/09_useRef/HookTimer';
+import DocumentTitle from './components/10_CustomHooks/01_useDT/DocumentTitle';
+import DocumentTitleTwo from './components/10_CustomHooks/01_useDT/DocumentTitleTwo';
+import Counter1 from './components/10_CustomHooks/02_useCounter/Counter';
 import Form from './components/10_CustomHooks/03_useInput/Form';
 
 function App() {
   return (
     <div className='App'>
-      {/* <HookCounter /> */}
-      {/* <HookCounterTwo /> */}
-      {/* <HookCounterThree /> */}
-      {/* <HookCounterFour /> */}
-      {/* <HookEffect /> */}
-      {/*  <HookEffectTwo /> */}
-      {/* <HookEffectThree /> */}
-      {/* <HookEffectFour /> */}
-      {/* <HookEffectFive /> */}
-      {/* <FetchData /> */}
-      {/* <FetchDataTwo /> */}
-      {/* <NameContext.Provider value={'Bilal'}>
+      <HookCounter />
+      <HookCounterTwo />
+      <HookCounterThree />
+      <HookCounterFour />
+      <HookEffect />
+      <HookEffectTwo />
+      <HookEffectThree />
+      <HookEffectFour />
+      <HookEffectFive />
+      <FetchData />
+      <FetchDataTwo />
+      <NameContext.Provider value={'Bilal'}>
         <AgeContext.Provider value={21}>
           <ComponentA />
         </AgeContext.Provider>
-      </NameContext.Provider> */}
-      {/* <HookReducer /> */}
-      {/* <HookReducerTwo /> */}
-      {/*  <Main /> */}
-      {/* <FetchingData /> */}
-      {/* <FetchingDataTwo /> */}
-      {/* <ParentComponent /> */}
-      {/* <Counter /> */}
-      {/* <FocusInput /> */}
-      {/* <HookTimer /> */}
-      {/*  <DocumentTitle />
-      <DocumentTitleTwo /> */}
-      {/* <Counter /> */}
+      </NameContext.Provider>
+      <HookReducer />
+      <HookReducerTwo />
+      <Main />
+      <FetchingData />
+      <FetchingDataTwo />
+      <ParentComponent />
+      <Counter />
+      <FocusInput />
+      <HookTimer />
+      <DocumentTitle />
+      <DocumentTitleTwo />
+      <Counter1 />
       <Form />
     </div>
   );
